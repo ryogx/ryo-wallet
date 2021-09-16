903d26457b024e44e28e8693515e50e658984e98
ryogx <89699088+ryogx@users.noreply.github.com>
2021-09-16 07:46:27 -0700

    Create installers

--- build_exe_installer.js
+++ build_exe_installer.js
@@ -1,17 +1,24 @@
+// This script makes a broken installer - DO NOT USE
+// use "build_msi_installer" instead.
 const electronInstaller = require('electron-winstaller');
 const path = require('path');
 
-async function createInstaller {
+async function createInstaller() {
   const APP_DIR = path.resolve(__dirname, './dist/electron-mat/Packaged/win-unpacked');
   const OUT_DIR = path.resolve(__dirname, './dist/electron-mat/win_installer');
   try {
     await electronInstaller.createWindowsInstaller({
       appDirectory: APP_DIR,
       outputDirectory: OUT_DIR,
+      name: 'RyoWalletAtom',
+      title: 'RyoWalletAtom',
       authors: 'Ryo Wallet Developers',
-      exe: 'Ryo Wallet Atom.exe'
+      setupExe: 'Ryo Wallet Atom Setup.exe',
+      setupMsi: 'Ryo Wallet Atom Setup.msi',
+      exe: 'Ryo Wallet Atom.exe',
+      version: '1.5.0'
     });
-    console.log(`Installer created at ${OUTD_DIR}`);
+    console.log(`Installer created at ${OUT_DIR}`);
   } catch (e) {
     console.log(`Failed to create installer: ${e.message}`);
   }
--- build_msi_installer.js
+++ build_msi_installer.js
@@ -1,17 +1,28 @@
 // ./build_installer.js
 
 // 1. Import Modules
-const { MSICreator } = require('electron-wix-msi');
+const { MSICreator } = require('@zenul_abidin/electron-wix-msi');
 const path = require('path');
+const fsPromises = require('fs/promises');
 
 // 2. Define input and output directory.
-// Important: the directories must be absolute, not relative e.g
-// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64",
-const APP_DIR = path.resolve(__dirname, './dist/electron-mat/Packaged/win-unpacked/');
-// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer",
-const OUT_DIR = path.resolve(__dirname, './windows_installer');
+// Important: the directories must be absolute, not relative.
+const APP_DIR = path.resolve(__dirname, './dist/electron-mat/Packaged/win-unpacked');
+const OUT_DIR = path.resolve(__dirname, './dist/electron-mat/windows_installer');
 
-// 3. Instantiate the MSICreator
+// 3, Copy LICENSE file into app directory or else the build will abort.
+async function copyLicenseFile() {
+  try {
+    await fsPromises.copyFile(path.resolve(__dirname, './LICENSE'), path.join(APP_DIR, './LICENSE'), 0)
+  } catch (err) {
+    process.stderr.write(`Could not copy LICENSE file to app directory (does it exist?): ${err}`)
+    process.exit()
+  }
+}
+
+copyLicenseFile()
+
+// 4. Instantiate the MSICreator
 const msiCreator = new MSICreator({
     appDirectory: APP_DIR,
     outputDirectory: OUT_DIR,
@@ -29,9 +40,10 @@ const msiCreator = new MSICreator({
     },
 });
 
-// 4. Create a .wxs template file
+// 5. Create a .wxs template file
 msiCreator.create().then(function(){
 
-    // Step 5: Compile the template to a .msi file
+    // 6: Compile the template to a .msi file
     msiCreator.compile();
 });
+
